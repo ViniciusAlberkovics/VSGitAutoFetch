41d7d61052299d10de83e0dbc7965dc340b9ddf5
Vinicius Alberkovics <viniciusa.d@hotmail.com>
2019-06-01 20:29:22 -0300

    Autofetch still not parametrivelable

---
+++ GitAutoFetch/AutoFetch.cs
@@ -0,0 +1,135 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Process = System.Diagnostics.Process;
+using Task = System.Threading.Tasks.Task;
+
+namespace GitAutoFetch
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class AutoFetch
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("1963cd03-55e3-41de-b505-6965e7873f22");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        private static Timer timerThread;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AutoFetch"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private AutoFetch(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            CommandID menuCommandID = new CommandID(CommandSet, CommandId);
+            MenuCommand menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static AutoFetch Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider => this.package;
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in AutoFetch's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new AutoFetch(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            try
+            {
+                ThreadHelper.ThrowIfNotOnUIThread();
+
+                timerThread = new Timer(new TimerCallback(ExecCmd), null, TimeSpan.FromSeconds(0), TimeSpan.FromSeconds(10));
+            }
+            catch (Exception ex)
+            {
+                VsShellUtilities.ShowMessageBox(
+                    this.package,
+                    ex.Message,
+                    "Error Extension - VS Git AutoFetch",
+                    OLEMSGICON.OLEMSGICON_CRITICAL,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+
+                timerThread?.Dispose();
+            }
+        }
+
+        private async void ExecCmd(object state)
+        {
+            try
+            {
+                DTE dte = await package.GetServiceAsync(typeof(DTE)).ConfigureAwait(false) as DTE;
+
+                if (dte != null && !string.IsNullOrWhiteSpace(dte.Solution.FullName))
+                {
+                    string solutionDir = Path.GetDirectoryName(dte.Solution.FullName);
+
+                    string execute = $@"/C cd {solutionDir}&git fetch";
+
+                    Process.Start(new ProcessStartInfo()
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = execute,
+                        CreateNoWindow = false
+                    }).WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- GitAutoFetch/GitAutoFetchPackage.cs
+++ GitAutoFetch/GitAutoFetchPackage.cs
@@ -25,6 +25,7 @@ namespace GitAutoFetch
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(GitAutoFetchPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
     public sealed class GitAutoFetchPackage : AsyncPackage
     {
         /// <summary>
@@ -46,6 +47,7 @@ namespace GitAutoFetch
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await AutoFetch.InitializeAsync(this);
         }
 
         #endregion
